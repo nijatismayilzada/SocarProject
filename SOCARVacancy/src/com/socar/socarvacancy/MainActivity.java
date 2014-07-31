afd58f9eeaac7b4b4f9c4674c99166d89e5da246
nijatismayilzada <nicat.ismayilzade@gmail.com>
2014-07-31 17:41:24 +0500

    vacancy db2 connection

--- SOCARVacancy/src/com/socar/socarvacancy/MainActivity.java
+++ SOCARVacancy/src/com/socar/socarvacancy/MainActivity.java
@@ -1,5 +1,9 @@
 package com.socar.socarvacancy;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,11 +13,14 @@ import android.view.MenuItem;
 public class MainActivity extends Activity {
 
 	DatabaseHandler db = new DatabaseHandler(this);
+	DatabaseHandlerVacancies db2 = new DatabaseHandlerVacancies(this);
+	ArrayList vacancyList;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		try {
 			if (db.getLogin(0).getNumber().toString().equals("0")) {
 				Intent theIntent = new Intent(getApplication(),
@@ -26,6 +33,23 @@ public class MainActivity extends Activity {
 			Intent theIntent = new Intent(getApplication(), LoginActivity.class);
 			startActivity(theIntent);
 			finish();
+		}
+
+		AsyncTaskWS newTask = new AsyncTaskWS(MainActivity.this,
+				"getVacancyList", getApplicationContext());
+		newTask.execute();
+
+		try {
+			vacancyList = newTask.get();
+			int noOfVacancies = vacancyList.size();
+			for (int i = 0; i < noOfVacancies; i++) {
+				Map<String, String> vacancy = newTask.get().get(i);
+				db2.addVacancy(new Vacancy(vacancy.get("ID"), vacancy
+						.get("name")));
+			}
+		} catch (InterruptedException | ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 	}// onCreate
@@ -53,8 +77,8 @@ public class MainActivity extends Activity {
 			return super.onOptionsItemSelected(item);
 		}
 	}
-	
-	public void logout(){
+
+	public void logout() {
 		db.updateLogin(new Login(0, "0"));
 		Intent theIntent = new Intent(getApplication(), LoginActivity.class);
 		startActivity(theIntent);
